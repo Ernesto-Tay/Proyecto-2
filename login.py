d3c55ff0256f94f97a3432f04b9ebfaad45f5d43
Santiagobrrrr <sjbatz@correo.url.edu.gt>
2025-10-19 13:23:59 -0600

    Mejora visual de login, se implementó mejor el uso de CustomTkinter para la realización de las UI

--- login.py
+++ login.py
@@ -1,45 +1,64 @@
-import tkinter as tk
-from tkinter import ttk, messagebox
+import customtkinter as ctk
+from tkinter import messagebox
 from main import get_conn, DataBase
+
+ctk.set_appearance_mode("light")
+ctk.set_default_color_theme("blue")
 
 DataBase.create_tables()
 
-root = tk.Tk()
-root.title("BAWIZ SYSTEM - Login")
+root = ctk.CTk()
+root.title("BAWIZ SYSTEM")
 root.geometry("600x400")
 root.resizable(False, False)
 
-frame = ttk.Frame(root, padding=20)
-frame.pack(expand=True)
+title = ctk.CTkLabel(root,text="INICIAR SESIÓN",font=("Open Sans", 32, "bold"),text_color="#111111")
+title.pack(pady=25)
+# frame nombre
+# espacio gris
+frame_nombre = ctk.CTkFrame(root, fg_color="#e0e0e0", corner_radius=20)
+frame_nombre.pack(pady=10, ipadx=15, ipady=10)
 
-ttk.Label(frame, text="INICIAR SESIÓN", font=("Open Sans", 24, "bold")).grid(row=0, column=0, columnspan=2, pady=20)
+frame_nombre.grid_columnconfigure(0, minsize=150)
+frame_nombre.grid_columnconfigure(1, minsize=300)
 
-ttk.Label(frame, text="Nombre:").grid(row=1, column=0, sticky="e", padx=10, pady=10)
-entry_name = ttk.Entry(frame, width=30)
-entry_name.grid(row=2, column=1, padx=10, pady=10)
+label_nombre = ctk.CTkLabel(frame_nombre, text="Nombre", font=("Open Sans", 14))
+label_nombre.grid(row=0, column=0, padx=(10, 5), pady=10, sticky="e")
 
-ttk.Label(frame, text="ID de usuario:").grid(row=2, column=0, sticky="e", padx=10, pady=10)
-entry_id = ttk.Entry(frame, width=30)
-entry_id.grid(row=1, column=1, padx=10, pady=10)
+entry_nombre = ctk.CTkEntry(frame_nombre,width=280,height=35,corner_radius=10,fg_color="white",border_color="#cfcfcf",text_color="black",font=("Open Sans", 12))
+entry_nombre.grid(row=0, column=1, padx=(5, 10), pady=10, sticky="w")
 
-def validar_usuario(tabla, campo_id, id_usuario, nombre):
+# frame id
+# espacio gris
+frame_id = ctk.CTkFrame(root, fg_color="#e0e0e0", corner_radius=20)
+frame_id.pack(pady=10, ipadx=15, ipady=10)
+
+frame_id.grid_columnconfigure(0, minsize=150)
+frame_id.grid_columnconfigure(1, minsize=300)
+
+label_id = ctk.CTkLabel(frame_id, text="ID", font=("Open Sans", 14))
+label_id.grid(row=0, column=0, padx=(10, 5), pady=10, sticky="e")
+
+entry_id = ctk.CTkEntry(frame_id,width=280,height=35,corner_radius=10,fg_color="white",border_color="#cfcfcf",text_color="black",font=("Open Sans", 12))
+entry_id.grid(row=0, column=1, padx=(5, 10), pady=10, sticky="w")
+
+def validate_user(table, campo_id, id_usuario, name):
     try:
         with get_conn() as conn:
             query = f"""
                 SELECT 1
-                FROM {tabla} t
+                FROM {table} t
                 JOIN users u ON t.user_id = u.user_id
                 WHERE t.{campo_id} = ? AND u.name = ?
             """
-            result = conn.execute(query, (id_usuario, nombre)).fetchone()
+            result = conn.execute(query, (id_usuario, name)).fetchone()
             return result is not None
     except Exception as e:
         messagebox.showerror("Error de BD", str(e))
         return False
 
-# botones
 def login_admin():
-    name = entry_name.get().strip()
+    name = entry_nombre.get().strip()
     user_id = entry_id.get().strip()
 
     if not user_id or not name:
@@ -50,13 +69,13 @@ def login_admin():
         messagebox.showerror("Error", "El ID de administrador debe iniciar con 'ADM'.")
         return
 
-    if validar_usuario("admins", "admin_id", user_id, name):
+    if validate_user("admins", "admin_id", user_id, name):
         messagebox.showinfo("Éxito", f"Bienvenido administrador {name}")
     else:
         messagebox.showerror("Error", "Credenciales incorrectas o usuario no registrado.")
 
 def login_collab():
-    name = entry_name.get().strip()
+    name = entry_nombre.get().strip()
     user_id = entry_id.get().strip()
 
     if not user_id or not name:
@@ -67,21 +86,22 @@ def login_collab():
         messagebox.showerror("Error", "El ID del colaborador debe iniciar con 'COL'.")
         return
 
-    if validar_usuario("collaborators", "collab_id", user_id, name):
+    if validate_user("collaborators", "collab_id", user_id, name):
         messagebox.showinfo("Éxito", f"Bienvenido colaborador {name}")
     else:
         messagebox.showerror("Error", "Credenciales incorrectas o usuario no registrado.")
 
-# Estilo de botones
-style = ttk.Style()
-style.configure("GrayButton.TButton",background="#9b9b9b",foreground="#000000",font=("Open Sans", 11, "underline"),padding=10,borderwidth=0,relief="flat")
 
-# Visualización
-style.map("GrayButton.TButton",background=[("active", "#c0c0c0"),("pressed", "#a9a9a9")])
-buttons = ttk.Frame(frame)
-buttons.grid(row=3, column=0, columnspan=2, pady=30)
+# general botones
+frame_botones = ctk.CTkFrame(root, fg_color="transparent")
+frame_botones.pack(pady=25)
 
-ttk.Button(buttons, text="Ingresar (admin)", style="GrayButton.TButton",command=login_admin).grid(row=0, column=0, padx=10)
-ttk.Button(buttons, text="Ingresar (collab)", style="GrayButton.TButton",command=login_collab).grid(row=0, column=1, padx=10)
+# boton admin
+btn_admin = ctk.CTkButton(frame_botones,text="Ingresar (Admin)",width=150,height=45,corner_radius=25,fg_color="#bfbfbf",hover_color="#a9a9a9",text_color="black",font=("Open Sans", 12, "underline"),command=login_admin)
+btn_admin.grid(row=0, column=0, padx=15)
+
+# boton collab
+btn_collab = ctk.CTkButton(frame_botones,text="Ingresar (Colab)",width=150,height=45,corner_radius=25,fg_color="#bfbfbf",hover_color="#a9a9a9",text_color="black",font=("Open Sans", 12, "underline"),command=login_collab)
+btn_collab.grid(row=0, column=1, padx=15)
 
 root.mainloop()
