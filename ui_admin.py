bf69aefa9cf90ec5221e5ae3d2df409ad6c51a99
Santiagobrrrr <sjbatz@correo.url.edu.gt>
2025-10-20 00:27:41 -0600

    creación de dos archivos .py para iniciar con los menús de admin y colaboradores, para que no hayan confusiones en cuanto a funcionamiento, es un poco mejor estructurado

---
+++ ui_admin.py
@@ -0,0 +1,11 @@
+import customtkinter as ctk
+
+ctk.set_appearance_mode("light")
+ctk.set_default_color_theme("blue")
+
+root = ctk.CTk()
+root.title("BAWIZ SYSTEM - ADMINISTRADOR")
+root.geometry("600x400")
+root.resizable(False, False)
+
+root.mainloop()
