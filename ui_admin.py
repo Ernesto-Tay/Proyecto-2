dd514058436199149f20f30c5747966f91ca6427
Santiagobrrrr <sjbatz@correo.url.edu.gt>
2025-10-20 20:58:32 -0600

    Se inició la creación del header para el panel de administración, donde se inició la función de create_header para manejar este estilo de nav-bar, pues actualmente solo se agrego el contenedor base del menú superior

--- ui_admin.py
+++ ui_admin.py
@@ -5,5 +5,8 @@ class AdminUI(ctk.CTkFrame):
         super().__init__(master, fg_color="white")
         self.master = master
         self.pack(expand=True, fill="both")
+        self.create_header()
 
-        ctk.CTkLabel(self, text="Panel del Administrador", font=("Open Sans", 32, "bold"), text_color="#111").pack(pady=40)
+    def create_header(self):
+        header = ctk.CTkFrame(self, fg_color="#e5e5e5", height=60, corner_radius=0)
+        header.pack(side="top", fill="x")
