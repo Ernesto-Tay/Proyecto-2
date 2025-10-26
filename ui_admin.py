58537322f20942b76086dbc9c026f19b7e080185
jorgeernestotayruiz <tayocruz1772@gmailcom>
2025-10-26 10:25:51 -0600

    Visualización de botones y buscador (aún no funcionales)

--- ui_admin.py
+++ ui_admin.py
@@ -1,6 +1,8 @@
 import customtkinter as ctk
 from main import get_conn
+import tkinter as tk
 from tkinter import ttk, messagebox, filedialog
+import calendar
 
 DB_NAME = "bawiz.db"
 
@@ -46,7 +48,13 @@ class AdminUI(ctk.CTkFrame):
                 return None
 
             cols = {"sales": ["sale_id", "time", "client", "products", "total"], "products": ["product_id", "name", "type", "description", "sale_price", "stock"], "clients": ["client_id", "name", "phone", "sale_price", "stock"], "collaborators": ["collab_id", "name", "phone", "position"], "providers":["provider_id", "name", "phone", "products"]}
-            headers = ", ".join([f'"{c}"' for c in [cols]])
+            titles_dict = {"sales": ["ID", "hora", "cliente asociado", "productos", "total"], "products": ["ID", "Nombre", "Tipo", "Descripción", "Precio", "Stock"], "clients": ["ID", "Nombre", "Teléfono", "precio venta", "Stock"], "collaborators": ["ID", "Nombre", "Teléfono", "Posición"], "providers":["ID", "Nombre", "Teléfono", "Productos asociados"]}
+            headers = cols[kind]
+            titles = titles_dict[kind]
+
+            months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+            years = [str(y) for y in range (2025, 2030)]
+            date_pop = False
 
             tree = ttk.Treeview(root, show="headings")
             tree.pack(fill = "both", expand = True)
@@ -54,7 +62,6 @@ class AdminUI(ctk.CTkFrame):
             for col in headers:
                 tree.heading(col, text=col)
                 tree.column(col, width= 800 // len(headers))
-
 
             cur.execute(f"SELECT * FROM {kind}")
             for row in cur.fetchall():
@@ -66,13 +73,58 @@ class AdminUI(ctk.CTkFrame):
             controls = ctk.CTkFrame(frame, relief="ridge", fg_color="transparent")
             controls.pack(fill="x", padx=8, pady=(4,8))
 
-            col_label = ctk.CTkLabel(controls, text="Filtro...", font=("Open Sans", 13))
-            col_label.pack(side="left", padx=10, pady=10)
-
-
+            filter_btn = False
+            date_btn = False
+            search_btn = False
+            back_btn = False
 
             if kind == "sales":
-                ttk.Label(controls, text="Filtro...")
+                filter_btn = ctk.CTkButton(controls, text = "Filtrar", width = 100, height = 36,  corner_radius=18, fg_color="white",hover_color="#f2f2f2", text_color="black", font=("Open Sans", 13, "bold"))
+                filter_btn.pack(side="left", padx=6)
+
+                date_btn = ctk.CTkButton(controls, text = "Fecha", width = 100, height = 36, corner_radius=18, fg_color="white",hover_color="#f2f2f2", text_color="black", font=("Open Sans", 13, "bold"))
+                date_btn.pack(side="left", padx=6)
+
+                search_btn = ctk.CTkEntry(controls, text = "Buscar...", width = 400, height = 36, corner_radius = 18, fg_color = "white", text_color = "grey", font=("Open Sans", 13, "bold"))
+                search_btn.pack(side="left", padx=6)
+
+                back_btn = ctk.CTkButton(controls, text = "Volver", width = 100, height = 36 , corner_radius=18, fg_color="white",hover_color="#f2f2f2", text_color="black", font=("Open Sans", 13, "bold"))
+                back_btn.pack(side="right", padx=6)
+
+            else:
+                filter_btn = ctk.CTkButton(controls, text="Filtrar", width=200, height=36, corner_radius=18,
+                                           fg_color="white", hover_color="#f2f2f2", text_color="black",
+                                           font=("Open Sans", 13, "bold"))
+                filter_btn.pack(side="left", padx=6)
+
+                search_btn = ctk.CTkEntry(controls, text="Buscar...", width=400, height=36, corner_radius=18,
+                                          fg_color="white", text_color="grey", font=("Open Sans", 13, "bold"))
+                search_btn.pack(side="left", padx=6)
+
+                back_btn = ctk.CTkButton(controls, text="Volver", width=100, height=36, corner_radius=18,
+                                         fg_color="white", hover_color="#f2f2f2", text_color="black",
+                                         font=("Open Sans", 13, "bold"))
+                back_btn.pack(side="right", padx=6)
+
+            def date_cb(date_button, callback = None, first_values = None):
+                col_cb = ctk.CTkComboBox(controls,values = titles, fg_color="transparent", width=100, button_color=("white","#f2f2f2"), text_color="black", font=("Open Sans", 12))
+                col_cb.set(headers[0])
+                col_cb.pack(side="left", padx=10, pady=10)
+
+                if date_pop and tk.Toplevel.winfo_exists(date_pop):
+                    try:
+                        date_pop.lift()
+                        return
+                    except Exception:
+                        pass
+
+                date_pop = tk.Toplevel(root)
+                date_pop.wm_overrideredirect(True)
+                date_pop.transient(root)
+                date_pop.attributes("-topmost", True)
+
+
+
 
 
             return
