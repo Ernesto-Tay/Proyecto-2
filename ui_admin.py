1e223e160ff4bf7c70dffd3eabfa9059486c7a78
Santiagobrrrr <sjbatz@correo.url.edu.gt>
2025-10-26 17:52:46 -0600

    Se guarda el nombre del último botón en el 'last_opened' esto para desplegar el submenú del boton que se presiono, asi se despliega el submenú de cualquiera

--- ui_admin.py
+++ ui_admin.py
@@ -8,6 +8,7 @@ class AdminUI(ctk.CTkFrame):
         self.pack(expand=True, fill="both")
         self.create_header()
         self.active_submenu = None
+        self.last_opened
 
     def create_header(self):
         header = ctk.CTkFrame(self, fg_color="#e0e0e0", height=60, corner_radius=0)
@@ -17,42 +18,51 @@ class AdminUI(ctk.CTkFrame):
         left = ctk.CTkFrame(header, fg_color="transparent")
         left.pack(side="left", padx=15, pady=10)
 
-        btn_colab = ctk.CTkButton(left, text="Colaboradores",width=130, height=36, corner_radius=18,fg_color="white", hover_color="#f2f2f2",text_color="black", font=("Open Sans", 13, "bold"),command=lambda: self.toggle_submenu(btn_colab))
+        btn_colab = ctk.CTkButton(left, text="Colaboradores", width=130, height=36,corner_radius=18, fg_color="white", hover_color="#f2f2f2",text_color="black", font=("Open Sans", 13, "bold"),command=lambda: self.toggle_submenu("colab", btn_colab))
         btn_colab.pack(side="left", padx=6)
 
-        btn_provider = ctk.CTkButton(left, text="Proveedores", width=130, height=36, corner_radius=18, fg_color="white",hover_color="#f2f2f2", text_color="black", font=("Open Sans", 13, "bold"))
+        btn_provider = ctk.CTkButton(left, text="Proveedores", width=130, height=36,corner_radius=18, fg_color="white", hover_color="#f2f2f2",text_color="black", font=("Open Sans", 13, "bold"),command=lambda: self.toggle_submenu("provider", btn_provider))
         btn_provider.pack(side="left", padx=6)
 
-        btn_sales = ctk.CTkButton(left, text="Ventas", width=130, height=36, corner_radius=18, fg_color="white",hover_color="#f2f2f2", text_color="black", font=("Open Sans", 13, "bold"))
+        btn_sales = ctk.CTkButton(left, text="Ventas", width=130, height=36,corner_radius=18, fg_color="white", hover_color="#f2f2f2",text_color="black", font=("Open Sans", 13, "bold"),command=lambda: self.toggle_submenu("sales", btn_sales))
         btn_sales.pack(side="left", padx=6)
 
-        btn_products = ctk.CTkButton(left, text="Productos", width=130, height=36, corner_radius=18, fg_color="white",hover_color="#f2f2f2", text_color="black", font=("Open Sans", 13, "bold"))
+        btn_products = ctk.CTkButton(left, text="Productos", width=130, height=36,corner_radius=18, fg_color="white", hover_color="#f2f2f2",text_color="black", font=("Open Sans", 13, "bold"),command=lambda: self.toggle_submenu("products", btn_products))
         btn_products.pack(side="left", padx=6)
 
-        btn_clients = ctk.CTkButton(left, text="Clientes", width=130, height=36, corner_radius=18, fg_color="white",hover_color="#f2f2f2", text_color="black", font=("Open Sans", 13, "bold"))
+        btn_clients = ctk.CTkButton(left, text="Clientes", width=130, height=36,corner_radius=18, fg_color="white", hover_color="#f2f2f2",text_color="black", font=("Open Sans", 13, "bold"),command=lambda: self.toggle_submenu("clients", btn_clients))
         btn_clients.pack(side="left", padx=6)
 
         # frame derecho
         right = ctk.CTkFrame(header, fg_color="transparent")
         right.pack(side="right", padx=15, pady=10)
 
-        btn_logout = ctk.CTkButton(right, text="Cerrar Sesión", width=130, height=36, corner_radius=18, fg_color="white",hover_color="#f2f2f2", text_color="black", font=("Open Sans", 13, "bold"), command = self.logout)
+        btn_logout = ctk.CTkButton(right, text="Cerrar Sesión", width=130, height=36,corner_radius=18, fg_color="white", hover_color="#f2f2f2",text_color="black", font=("Open Sans", 13, "bold"),command=self.logout)
         btn_logout.pack(side="right", padx=6)
 
-    def toggle_submenu(self, parent_button):
-        if self.active_submenu:     # Guarda el submenú actualmente visible. Si ya hay uno, se desaperece o se destruye antes de abrir otro.
+    def toggle_submenu(self, name, parent_button):
+        if self.active_submenu: # Guarda el submenú, si ya hay uno, se desaperece o se destruye antes de abrir otro.
             self.active_submenu.destroy()
             self.active_submenu = None
-            return
+
+            if self.last_opened == name:
+                self.last_opened = None
+                return
 
         # frame submenu
-        submenu = ctk.CTkFrame(self, fg_color="#f8f8f8",corner_radius=12, border_width=1, border_color="#d0d0d0" )
+        submenu = ctk.CTkFrame(self, fg_color="#f8f8f8", corner_radius=12,border_width=1, border_color="#d0d0d0")
+        submenu.place(x=parent_button.winfo_rootx() - self.master.winfo_rootx(),y=parent_button.winfo_rooty() - self.master.winfo_rooty() + 50)
 
-        submenu.place( x=parent_button.winfo_rootx() - self.master.winfo_rootx(),y=parent_button.winfo_rooty() - self.master.winfo_rooty() + 50) #Coloca el submenú  debajo del botón
+        if name == "colab":
+            self.add_submenu_button(submenu, "Agregar colaborador",lambda: self.action("Agregar colaborador"))
+            self.add_submenu_button(submenu, "Ver colaboradores",lambda: self.action("Ver colaboradores"))
 
-        self.add_submenu_button(submenu, "Agregar colaborador",lambda: self.action("Agregar colaborador"))
-        self.add_submenu_button(submenu, "Ver colaboradores",lambda: self.action("Ver colaboradores"))
-        self.active_submenu = submenu # Guarda la referencia en submenu
+        elif name == "provider":
+            self.add_submenu_button(submenu, "Agregar proveedor",lambda: self.action("Agregar proveedor"))
+            self.add_submenu_button(submenu, "Ver proveedores",lambda: self.action("Ver proveedores"))
+
+        self.active_submenu = submenu # Guardam la referencia en submenu
+        self.last_opened = name
 
     def add_submenu_button(self, parent, text, command):
         btn = ctk.CTkButton(parent, text=text, width=180, height=30,fg_color="#ffffff", hover_color="#e6e6e6",text_color="black", font=("Open Sans", 12),command=command)
